d55bf7eec0db6cea46994fb3e50213ec8d9b9f81
HUMZA Tareen <humzakhawartareen@gmail.com>
2022-05-18 15:25:12 +0500

    updated gitignore and created recommendation screen

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Dashboard from "./Dashboard";
 import "./LoginScreen";
 import Login from "./LoginScreen";
+import RecommendationScreen from "./RecommendationScreen";
 // import Navbar from "./navbar";
 import Registration from "./Registration";
 
 function App() {
   return (
     <>
-      <Login />;
+      <RecommendationScreen />;
     </>
   );
 }
