3622888d795aba0849968c73aa9bf589a424a071
mianafraf <mianafraf@gmail.com>
2022-05-18 14:36:23 +0500

    added form

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,6 +1,43 @@
 import React from "react";
 import "./App.css";
 export default function LoginScreen() {
+
+  const [state, updateState] = React.useState({
+    username: "",
+    password: "",
+    return: 0
+  });
+
+  function onChangeValue(event) {
+    updateState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+   
+    console.log(state);
+  }
+  
+
+  React.useState(
+    () => {
+      fetch("http://localhost/login.php")
+      .then(res => res.text())
+      .then(res => {
+        updateState({
+          ...state,
+          return: res
+        });
+      });
+    },
+    [state.username]
+  );
+  
+
+
   return (
     <div className="loginBody">
       <div className="outerDiv">
@@ -14,20 +51,25 @@ export default function LoginScreen() {
           <div className="titleDiv">
             <h1 className="loginTitle">
               Pharmacotherapy for Obesity Management
+              {state.return}
             </h1>
           </div>
           <div className="loginDetailsDiv">
             <h2 className="title2">Welcome,</h2>
           </div>
+          <form method="post" action="http://localhost/login.php" onSubmit={handleSubmit}>  
           <div className="loginFieldsDiv">
             <h3>Username</h3>
-            <input type="text" />
+            <input type="text" name="username" onChange={onChangeValue} />
             <h3>Password</h3>
-            <input type="password" />
+            <input type="password" name="password" onChange={onChangeValue} />
           </div>
+          
           <div className="loginButtonDiv">
             <button title="Login">Login</button>
           </div>
+          </form>
+          
         </div>
       </div>
     </div>
