73f8ffe9661d11e9cba5d2a8971bbb17a3635fda
safi50 <63853856+safi50@users.noreply.github.com>
2022-05-18 16:54:52 +0500

    Table Components for Report and Rec screens

---
+++ src/ReportTable.js
@@ -0,0 +1,22 @@
+import "./PatientReport.css";
+
+
+const ReportTable = () => {
+    return (
+        <div className="report--table">
+            <span>CBC wdwdwdWBC: <input type="text" className="report--input" /></span>
+            <span>CBC WBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC WBC: <input type="text" className="report--input" /></span>
+            <span>CBC WdwdwBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC WdwdwBC: <input type="text" className="report--input" /></span>
+            <span>CBC dwddWBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC dwdwdwdwdWBC: <input type="text" className="report--input" /></span>
+        </div>
+    );
+}
+export default ReportTable;
+
+
---
+++ src/ReportTable2.js
@@ -0,0 +1,31 @@
+import "./PatientReport.css";
+
+const ReportTable2 = () => {
+    return (
+        <div className="report--table2">
+            <span>CBCwdwdwdwd WBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBCaaw WBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC wawdaccWBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC ccWBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC WBdwacacawC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC cwcwadwWBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC WwcacascBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC cccWBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC WBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC WccccBC: <input type="text" className="report--input" /></span>
+            <br></br> <br></br>
+            <span>CBC WwBC: <input type="text" className="report--input" /></span>
+        </div>
+    );
+}
+
+export default ReportTable2;
