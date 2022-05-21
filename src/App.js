589d4551a2cb464edce0ec1f0853880614103f4e
mianafraf <mianafraf@gmail.com>
2022-05-21 11:27:34 +0500

    created php for login screen

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Dashboard from "./Dashboard";
 import "./LoginScreen";
 import Login from "./LoginScreen";
-// import Navbar from "./navbar";
+import Navbar from "./navbar";
+import PatientHeader from "./PatientHeader";
+import PatientReport from "./PatientReport";
 import Registration from "./Registration";
 
+
 function App() {
+
   return (
     <>
-      <Login />;
+    <Login />
     </>
   );
 }
