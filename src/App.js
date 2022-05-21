bb61036c9ecc7c750becbe93a5cca47aaf668f5b
HUMZA Tareen <humzakhawartareen@gmail.com>
2022-05-21 13:02:08 +0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
 import Dashboard from "./Dashboard";
-import "./LoginScreen";
-import Login from "./LoginScreen";
-import RecommendationScreen from "./RecommendationScreen";
-// import Navbar from "./navbar";
+import PatientReport from "./PatientReport";
 import Registration from "./Registration";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import LoginScreen from "./LoginScreen";
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <>
-      <RecommendationScreen />;
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/reg" element={<Registration />} />
+        <Route path="/dash" element={<Dashboard />} />
+        <Route path="/report" element={<PatientReport />} />
+      </Routes>
+    </Router>
   );
 }
 
