2e519c5f03acacd040fdabf0fcba0f102e4f1fe1
Saad Subhani <saad.subhani123456@gmail.com>
2022-05-22 22:55:34 +0500

    Laal HAI

--- search.php
+++ search.php
@@ -1,7 +1,25 @@
 <?php
+ header('Access-Control-Allow-Origin: http://localhost:3000');
+$search="Khawar Boink";
 
-$search=$_POST["search"];
-
-
+$conn = new mysqli("localhost", "root", "", "obesity silo");
+$sql1 = "SELECT * FROM patientprofile WHERE patientName = '$search'";
+$result1 = $conn->query($sql1);
+if ($result1 != null) {
+    while ($row1 = $result1->fetch_assoc()) {
+        echo $row1["patientId"];
+        echo '<br>';
+            echo $row1["patientName"];
+            echo '<br>';
+            echo $row1["patientName"];
+            echo '<br>';
+            echo $row1["gender"];
+            echo '<br>';
+            echo $row1["dateOfBirth"];
+    }
+}
+else{
+    echo "Patient not found";
+}
 
 ?>
