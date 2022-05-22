6aee9912c4c173dfd5b01d1b84c6c6d004e2c671
Saad Subhani <saad.subhani123456@gmail.com>
2022-05-22 23:11:56 +0500

    Khawar Boink search successful

--- search.php
+++ search.php
@@ -2,21 +2,17 @@
  header('Access-Control-Allow-Origin: http://localhost:3000');
 $search="Khawar Boink";
 
+
+
 $conn = new mysqli("localhost", "root", "", "obesity silo");
 $sql1 = "SELECT * FROM patientprofile WHERE patientName = '$search'";
 $result1 = $conn->query($sql1);
 if ($result1 != null) {
     while ($row1 = $result1->fetch_assoc()) {
-        echo $row1["patientId"];
-        echo '<br>';
-            echo $row1["patientName"];
-            echo '<br>';
-            echo $row1["patientName"];
-            echo '<br>';
-            echo $row1["gender"];
-            echo '<br>';
-            echo $row1["dateOfBirth"];
+            $json['data'][]=array('name'=> $row1["patientName"], 'gender'=>$row1["gender"]);
     }
+    $display= json_encode($json);
+    echo $display;
 }
 else{
     echo "Patient not found";
