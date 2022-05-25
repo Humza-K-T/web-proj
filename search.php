a0071d199b4219728a3f95c026d7f5525ccca02e
mianafraf <73768187+mianafraf@users.noreply.github.com>
2022-05-25 19:56:31 +0500

    Update Search.php

--- search.php
+++ search.php
@@ -1,21 +1,27 @@
 <?php
  header('Access-Control-Allow-Origin: http://localhost:3000');
-$search="Khawar Boink";
 
 
+ $pID = $_POST['search'];
 
-$conn = new mysqli("localhost", "root", "", "obesity silo");
-$sql1 = "SELECT * FROM patientprofile WHERE patientName = '$search'";
+$conn = new mysqli("localhost", "root", "", "obesity_silo");
+$sql1 = "SELECT * FROM `patientprofile` WHERE patientId = '$pID'";
 $result1 = $conn->query($sql1);
-if ($result1 != null) {
-    while ($row1 = $result1->fetch_assoc()) {
-            $jsn['dat'][]=array('Id'=> $row1["patientId"],'name'=> $row1["patientName"], 'gender'=>$row1["gender"]);
-    }
-    $display= json_encode($jsn);
-    echo $display;
+
+// if ($result1->num_rows > 0) {
+//     while ($row1 = $result1->fetch_assoc()) {
+
+//         $json['data'][]=array('Id'=> $row1["patientId"],'name'=> $row1["patientName"], 'gender'=>$row1["gender"], 'Registration_Date'=>$row1["dateOfRegistration"]);
+//     }
+//     $display= json_encode($json);
+//     echo $display;
+
+//     }
+
+if($pID < 3){
+    echo $pID;
 }
 else{
-    echo "Patient not found";
+    echo "error";
 }
-
-?>
+$conn->close();
